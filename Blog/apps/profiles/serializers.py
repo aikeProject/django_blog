4bf4c94dd048178821215435301b3a13fa94f23d
成雨 <rensailong@zhongan.io>
2019-10-16 18:19:59 +0800

    update

--- Blog/apps/profiles/serializers.py
+++ Blog/apps/profiles/serializers.py
@@ -17,7 +17,7 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     username = serializers.CharField(source='user.username')
     bio = serializers.CharField(allow_blank=True, required=False)
-    image = serializers.URLField(allow_blank=True, required=False)
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
