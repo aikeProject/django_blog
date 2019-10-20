d2788b2769f5a7c44cf157de8d3c73e5a02c61a2
成雨 <rensailong@zhongan.io>
2019-10-20 19:14:34 +0800

    article

--- Blog/apps/articles/views.py
+++ Blog/apps/articles/views.py
@@ -1,9 +1,9 @@
-from rest_framework import mixins, status, viewsets
-from rest_framework.exceptions import NotFound
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.settings import api_settings
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, UpdateModelMixin, RetrieveModelMixin
 
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
@@ -11,15 +11,16 @@ from .models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleViewSet(mixins.CreateModelMixin,
-                     mixins.ListModelMixin,
-                     mixins.RetrieveModelMixin,
+class ArticleViewSet(CreateModelMixin,
+                     ListModelMixin,
+                     RetrieveModelMixin,
+                     UpdateModelMixin,
                      viewsets.GenericViewSet):
     """
-
+    文章
     """
     lookup_field = 'slug'
-    queryset = Article.objects.select_related('author', 'author__user')
+    queryset = Article.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = ArticleSerializer
     authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
@@ -39,29 +40,3 @@ class ArticleViewSet(mixins.CreateModelMixin,
             return {'Location': str(data[api_settings.URL_FIELD_NAME])}
         except (TypeError, KeyError):
             return {}
-
-    def retrieve(self, request, slug):
-        try:
-            serializer_instance = self.queryset.get(slug=slug)
-        except Article.DoesNotExist:
-            raise NotFound('没有此文章')
-
-        serializer = self.serializer_class(serializer_instance)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, slug):
-        try:
-            serializer_instance = self.queryset.get(slug=slug)
-        except Article.DoesNotExist:
-            raise NotFound('没有此文章，无法修改')
-
-        serializer_data = request.data
-
-        serializer = self.serializer_class(
-            serializer_instance, data=serializer_data, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
