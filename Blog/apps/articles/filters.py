45d85f818a937a0c8466bfb8679901be2f341bb4
成雨 <rensailong@zhongan.io>
2019-11-03 11:55:25 +0800

    update 过滤查询 Description

--- Blog/apps/articles/filters.py
+++ Blog/apps/articles/filters.py
@@ -16,9 +16,11 @@ from django_filters import rest_framework, UUIDFilter, CharFilter
 
 
 class ArticlesFilter(rest_framework.FilterSet):
-    uid = UUIDFilter(field_name="author__uid")
-    tagId = CharFilter(field_name='article2tag__tag__id')
-    webCategoryId = CharFilter(method='category_filer')
+    uid = UUIDFilter(field_name="author__uid", help_text='作者uid')
+    tagId = CharFilter(field_name='article2tag__tag__id', help_text='标签id')
+    category = CharFilter(field_name='category', help_text='个人文章分类')
+    web_category = CharFilter(field_name='web_category', help_text='网站分类，只能查子级分类')
+    webCategoryId = CharFilter(method='category_filer', help_text='网站分类，子级和父级分类都能查')
 
     # 网站分类的父级和子级均可过滤
     def category_filer(self, queryset, name, value):
@@ -26,7 +28,7 @@ class ArticlesFilter(rest_framework.FilterSet):
 
     class Meta:
         model = Article
-        fields = ('uid', 'category', 'tagId', 'web_category')
+        fields = ()
 
 
 class TagFilter(rest_framework.FilterSet):
